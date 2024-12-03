6dbd0a7f339e751ece4ca8930a3c32a96beaf389
TDIR Admin <splunker>
2024-12-03 01:20:31 +0000

    testing

--- Risk Finding - Related Findings.py
+++ Risk Finding - Related Findings.py
@@ -589,16 +589,16 @@ def related_findings_list(action=None, success=None, container=None, results=Non
     for item in run_query_1_result_item_0:
         if len([item]) != 1:
             for i in range(len(item)):
-                related_findings_list__related_findings_id.append(item[i])
+                related_findings_list__related_findings_id.append(item[0][i])
         else:
-            related_findings_list__related_findings_id.append([item])
+            related_findings_list__related_findings_id.append([item][0])
     
     for item in run_query_1_result_item_1:
         if len([item]) != 1:
             for i in range(len(item)):
-                related_findings_list__related_findings_time.append(item[i])
+                related_findings_list__related_findings_time.append(item[0][i])
         else:
-            related_findings_list__related_findings_time.append([item])
+            related_findings_list__related_findings_time.append([item][0])
             
     phantom.debug(related_findings_list__related_findings_id)
     phantom.debug(related_findings_list__related_findings_time)
