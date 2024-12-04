db83605501715adb8274b05b223a6bb070cc67cb
TDIR Admin <splunker>
2024-12-04 06:35:14 +0000

    test

--- Risk Finding - Related Findings.py
+++ Risk Finding - Related Findings.py
@@ -758,15 +758,17 @@ def get_finding_or_investigation_1(action=None, success=None, container=None, re
 
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
-    related_findings_list__related_findings_id = json.loads(_ if (_ := phantom.get_run_data(key="related_findings_list:related_findings_id")) != "" else "null")  # pylint: disable=used-before-assignment
+    id_list__result = phantom.collect2(container=container, datapath=["id_list:custom_function_result.data.output"])
 
     parameters = []
 
-    if related_findings_list__related_findings_id is not None:
-        parameters.append({
-            "id": related_findings_list__related_findings_id,
-            "finding_time": "",
-        })
+    # build parameters list for 'get_finding_or_investigation_1' call
+    for id_list__result_item in id_list__result:
+        if id_list__result_item[0] is not None:
+            parameters.append({
+                "id": id_list__result_item[0],
+                "finding_time": "",
+            })
 
     ################################################################################
     ## Custom Code Start
