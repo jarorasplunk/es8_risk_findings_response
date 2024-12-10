9b7a6e238e95f7f349eb4968a21771d0543f82a8
TDIR Admin <splunker>
2024-12-10 14:05:09 +0000

    test

--- Risk Finding - Investigate Findings.py
+++ Risk Finding - Investigate Findings.py
@@ -428,10 +428,10 @@ def threat_list(action=None, success=None, container=None, results=None, handle=
     ################################################################################
 
     # Write your custom code here...
-    phantom.debug(run_query_1_result_item_0[0])
-    phantom.debug(run_query_1_result_item_1[0])
+    phantom.debug(run_query_1_result_item_0)
+    phantom.debug(run_query_1_result_item_1)
     threat_list__threat_list = []
-    threat_list__threat_list = [list(pair) for pair in zip(run_query_1_result_item_0[0], run_query_1_result_item_1[0])]
+    threat_list__threat_list = [list(pair) for pair in zip(run_query_1_result_item_0, run_query_1_result_item_1)]
     
     phantom.debug(threat_list__threat_list)
     ################################################################################
