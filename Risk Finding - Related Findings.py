9c4a3fc7257fdfde3cac2b089736aea366042856
Jit Arora <jitaror@cisco.com>
2025-06-19 02:46:01 +0000

    test

--- Risk Finding - Related Findings.py
+++ Risk Finding - Related Findings.py
@@ -887,7 +887,7 @@ def included_findings(action=None, success=None, container=None, results=None, h
 
     final_result = {}
 
-    for item in get_finding_or_investigation_1_result_item_0:
+    for item in get_finding_or_investigation_1_result_item_0[0]:
         result = {
             "finding_ids": [],
             "intermediate_finding_ids": []
