f5b529fb4d3275e9772e0b3fa300e2f77ab13a3d
Jit Arora <jitaror@cisco.com>
2025-06-24 03:52:00 +0000

    test

--- Risk Finding - Related Findings.py
+++ Risk Finding - Related Findings.py
@@ -1139,7 +1139,7 @@ def open_findings_format(action=None, success=None, container=None, results=None
             finding_url = "https://i-0e6bc36a44836889b.splunk.show/en-GB/app/SplunkEnterpriseSecuritySuite/incident_review?earliest=-30d&latest=now&event_id=" + event_id
             #status = status.replace('\n','')
             #owner = owner.replace('\n','')
-            if status!='Closed' or status!='Resolved':
+            if status not in ('Closed', 'Resolved'):
                 phantom.debug(finding_url)
                 phantom.debug(status)
                 note += "|{}|[{}]({})|{}|{}|\n".format(rule_name, event_id, finding_url, status, owner)
