ced110a632780c7fd83c6d2502c17ba2b9f1af6b
dluxton@splunk.com <dluxton@splunk.com>
2025-06-25 09:06:17 +0000

    yes

--- Random Triage: Get Open Alerts.py
+++ Random Triage: Get Open Alerts.py
@@ -94,8 +94,8 @@ def list_open_alerts(action=None, success=None, container=None, results=None, ha
     parameters.append({
         "limit": 2,
         "disposition": "Undetermined",
-        "latest": "-15m@m",
-        "earliest": "-24h",
+        "latest": "-24h",
+        "earliest": "-7d",
     })
 
     ################################################################################
